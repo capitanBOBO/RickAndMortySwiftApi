a53d5be4fafdb9382bdfaaeeefbef61476b0a531
Ivan Savin <Ivan.Savin@excelian.com>
2022-06-21 22:47:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# rickAndMortySwiftApi
+
+A description of this package.
---
+++ Sources/rickAndMortySwiftApi/rickAndMortySwiftApi.swift
@@ -0,0 +1,6 @@
+public struct rickAndMortySwiftApi {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
